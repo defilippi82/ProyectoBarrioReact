d7d90158f36e485973908e5f17a76cf34ba9635f
Federico H. De Filippi <53954795+defilippi82@users.noreply.github.com>
2024-06-21 09:28:17 -0300

    FIREBASE MESSAGE CLOUD

--- src/firebaseConfig/firebase.js
+++ src/firebaseConfig/firebase.js
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore, collection, addDoc, onSnapshot } from "firebase/firestore";
+import { getMessaging } from "firebase/messaging";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -32,5 +33,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
+// Initialize Firebase Cloud Messaging and get a reference to the service
+export const messaging = getMessaging(app);
 
 
