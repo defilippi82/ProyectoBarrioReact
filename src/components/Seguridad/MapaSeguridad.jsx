b6a0fe1518ea0d068573be221a4a132234f71ed3
defilippi82 <f.defilippi@gmail.com>
2024-06-25 08:58:24 -0300

    seguridad

--- src/components/Seguridad/MapaSeguridad.jsx
+++ src/components/Seguridad/MapaSeguridad.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-//import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-//import 'leaflet/dist/leaflet.css';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
 
 export const MapaSeguridad = () => {
   const unidades = [
