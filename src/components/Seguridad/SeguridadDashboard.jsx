ab2a214de2c7d95355aec1997c46551078f7da00
Federico H. De Filippi <53954795+defilippi82@users.noreply.github.com>
2025-01-26 09:04:23 -0300

    seguridad2

--- src/components/Seguridad/SeguridadDashboard.jsx
+++ src/components/Seguridad/SeguridadDashboard.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import {MapaSeguridad} from './MapaSeguridad2';
+import {MapaSeguridad2} from './MapaSeguridad2';
 import  { Mensajeria} from "../Services/Mensajeria";
 
 export const SeguridadDashboard = () => {
   return (
     <div>
       <h1>Panel de Seguridad</h1>
-      <MapaSeguridad/>
+      <MapaSeguridad2/>
       <Mensajeria/>
       </div>
   );
