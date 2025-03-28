29a7c725319b24a71f7b44534c4a191813b9fd05
defilippi82 <f.defilippi@gmail.com>
2025-03-28 10:21:34 -0300

    Novedades

--- src/components/Services/Novedades.jsx
+++ src/components/Services/Novedades.jsx
@@ -1,180 +1,147 @@
 import React, { useState, useEffect, useContext } from 'react';
-import { collection, getDocs, addDoc, deleteDoc, updateDoc, doc } from 'firebase/firestore';
+import { collection, getDocs, addDoc, deleteDoc, doc } from 'firebase/firestore';
 import { db } from '/src/firebaseConfig/firebase.js';
 import { UserContext } from '../Services/UserContext';
-import {Card, Button,Form, Row, Col} from 'react-bootstrap';
+import { Card, Button, Tabs, Tab, Row, Col, Form } from 'react-bootstrap';
 import Swal from 'sweetalert2';
 import withReactContent from 'sweetalert2-react-content';
+import { FaStar } from 'react-icons/fa';
 
 const MySwal = withReactContent(Swal);
+
+const getRandomColor = () => {
+    const colors = ['#FFB6C1', '#ADD8E6', '#90EE90', '#FFD700', '#FF6347'];
+    return colors[Math.floor(Math.random() * colors.length)];
+};
 
 export const Novedades = () => {
     const { userData } = useContext(UserContext);
     const [novedades, setNovedades] = useState([]);
-    const [newNovedad, setNewNovedad] = useState('');
-    const [newImage, setNewImage] = useState(null);
+    const [telefonosUtiles, setTelefonosUtiles] = useState([]);
+    const [key, setKey] = useState('novedades');
+    const [rating, setRating] = useState({});
+    const [categoriaSeleccionada, setCategoriaSeleccionada] = useState('Jardinero');
+    const [categorias, setCategorias] = useState(["Jardinero", "Piletero", "Plomero", "Electricista", "Técnico informático", "Sodero", "Cable e internet", "Veterinarias", "Pozero"]);
 
     useEffect(() => {
         const fetchNovedades = async () => {
             const querySnapshot = await getDocs(collection(db, 'novedades'));
-            const data = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-            data.sort((a, b) => new Date(a.fecha.split('/').reverse().join('-')) - new Date(b.fecha.split('/').reverse().join('-')));
-            setNovedades(data);
+            setNovedades(querySnapshot.docs.map(doc => ({ id: doc.id, color: getRandomColor(), ...doc.data() })));
         };
-
+        const fetchTelefonos = async () => {
+            const querySnapshot = await getDocs(collection(db, 'telefonosUtiles'));
+            setTelefonosUtiles(querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() })));
+        };
         fetchNovedades();
+        fetchTelefonos();
     }, []);
 
-    const handleAddNovedad = async (e) => {
-        e.preventDefault();
-        const currentDate = new Date();
-        const formattedDate = `${String(currentDate.getDate()).padStart(2, '0')}/${String(currentDate.getMonth() + 1).padStart(2, '0')}/${currentDate.getFullYear()}`;
-        
-        if (newNovedad.trim() === '') return;
-        const newEntry = {
-            socio: userData.nombre,
-            fecha: formattedDate,
-            novedad: newNovedad,
-            imagen: newImage,
-            likes: 0
-        };
-        try {
-            const docRef = await addDoc(collection(db, 'novedades'), newEntry);
-            setNovedades([...novedades, { id: docRef.id, ...newEntry }]);
-            setNewNovedad('');
-            setNewImage(null);
-            MySwal.fire('Éxito', 'Novedad agregada correctamente', 'success');
-        } catch (error) {
-            console.error('Error adding document: ', error);
-            MySwal.fire('Error', 'Hubo un problema al agregar la novedad', 'error');
+    const handleAddTelefono = async () => {
+        let nuevaCategoria = '';
+
+        const { value } = await MySwal.fire({
+            title: 'Agregar Teléfono',
+            html: `
+                <select id="swal-categoria" class="swal2-input">
+                    ${categorias.map(cat => `<option value="${cat}">${cat}</option>`).join('')}
+                    <option value="nueva">Nueva Categoría</option>
+                </select>
+                <input id="swal-nueva-categoria" class="swal2-input" placeholder="Nueva Categoría" style="display: none;">
+                <input id="swal-nombre" class="swal2-input" placeholder="Nombre">
+                <input id="swal-telefono" class="swal2-input" placeholder="Teléfono">
+                <input id="swal-email" class="swal2-input" placeholder="Email">
+            `,
+            didOpen: () => {
+                const selectCategoria = document.getElementById('swal-categoria');
+                const inputNuevaCategoria = document.getElementById('swal-nueva-categoria');
+
+                selectCategoria.addEventListener('change', (e) => {
+                    if (e.target.value === 'nueva') {
+                        inputNuevaCategoria.style.display = 'block';
+                    } else {
+                        inputNuevaCategoria.style.display = 'none';
+                    }
+                });
+            },
+            preConfirm: () => {
+                const categoria = document.getElementById('swal-categoria').value;
+                if (categoria === 'nueva') {
+                    nuevaCategoria = document.getElementById('swal-nueva-categoria').value;
+                    if (!categorias.includes(nuevaCategoria)) {
+                        setCategorias([...categorias, nuevaCategoria]);
+                    }
+                }
+                return {
+                    categoria: categoria === 'nueva' ? nuevaCategoria : categoria,
+                    nombre: document.getElementById('swal-nombre').value,
+                    telefono: document.getElementById('swal-telefono').value,
+                    email: document.getElementById('swal-email').value
+                };
+            }
+        });
+
+        if (value) {
+            await addDoc(collection(db, 'telefonosUtiles'), value);
+            setTelefonosUtiles([...telefonosUtiles, value]);
         }
     };
 
-    const handleDeleteNovedad = async (id) => {
-        MySwal.fire({
-            title: '¿Estás seguro?',
-            text: 'No podrás revertir esto',
-            icon: 'warning',
-            showCancelButton: true,
-            confirmButtonColor: '#d33',
-            cancelButtonColor: '#3085d6',
-            confirmButtonText: 'Sí, bórralo',
-            cancelButtonText: 'Cancelar'
-        }).then(async (result) => {
-            if (result.isConfirmed) {
-                try {
-                    await deleteDoc(doc(db, 'novedades', id));
-                    setNovedades(novedades.filter(novedad => novedad.id !== id));
-                    MySwal.fire('Borrado', 'La novedad ha sido borrada', 'success');
-                } catch (error) {
-                    console.error('Error deleting document: ', error);
-                    MySwal.fire('Error', 'Hubo un problema al borrar la novedad', 'error');
-                }
-            }
-        });
-    };
-
-    const handleEditNovedad = (id, currentNovedad) => {
-        MySwal.fire({
-            title: 'Editar Novedad',
-            input: 'textarea',
-            inputLabel: 'Novedad',
-            inputValue: currentNovedad,
-            showCancelButton: true,
-            confirmButtonText: 'Guardar',
-            cancelButtonText: 'Cancelar',
-            preConfirm: async (newNovedad) => {
-                if (newNovedad.trim() === '') {
-                    Swal.showValidationMessage('La novedad no puede estar vacía');
-                    return false;
-                }
-                try {
-                    const novedadDoc = doc(db, 'novedades', id);
-                    await updateDoc(novedadDoc, { novedad: newNovedad });
-                    setNovedades(novedades.map(n => n.id === id ? { ...n, novedad: newNovedad } : n));
-                    MySwal.fire('Éxito', 'Novedad actualizada correctamente', 'success');
-                } catch (error) {
-                    console.error('Error updating document: ', error);
-                    MySwal.fire('Error', 'Hubo un problema al actualizar la novedad', 'error');
-                }
-            }
-        });
-    };
-
-    const handleLike = async (id, currentLikes) => {
-        try {
-            const novedadDoc = doc(db, 'novedades', id);
-            await updateDoc(novedadDoc, { likes: currentLikes + 1 });
-            setNovedades(novedades.map(n => n.id === id ? { ...n, likes: currentLikes + 1 } : n));
-        } catch (error) {
-            console.error('Error updating document: ', error);
-            MySwal.fire('Error', 'Hubo un problema al dar like a la novedad', 'error');
+    const handleDelete = async (id, type) => {
+        await deleteDoc(doc(db, type, id));
+        if (type === 'novedades') {
+            setNovedades(novedades.filter(novedad => novedad.id !== id));
+        } else {
+            setTelefonosUtiles(telefonosUtiles.filter(telefono => telefono.id !== id));
         }
-    };
-
-    const handleContact = (socio) => {
-        MySwal.fire({
-            title: 'Contactar Socio',
-            text: `Puedes contactar a ${socio} a través del sistema de mensajería interno.`,
-            icon: 'info',
-            confirmButtonText: 'Entendido'
-        });
     };
 
     return (
         <div className="container mt-4">
-            <h2>Novedades / Avisos</h2>
-            <div className="row row-cols-1 row-cols-md-2 row-cols-sm-3 g-4">
-                {novedades.map(({ id, socio, fecha, novedad, imagen, likes }) => (
-                    <div className="col auto" key={id}>
-                    <Card key={id} style={{ width: 'auto', margin: 'auto' }}>
-                        {imagen && <Card.Img variant="top" src={imagen} />}
-                        <Card.Body>
-                            <Card.Title>{socio} Lote:{userData.manzana}-{userData.lote}</Card.Title>
-                            <Card.Subtitle className="mb-2 text-muted">{fecha}</Card.Subtitle>
-                            <Card.Text>{novedad}</Card.Text>
-                            <Button variant="primary" onClick={() => handleLike(id, likes)}>Like ({likes})</Button>
-                            {' '}
-                            <Button variant="info" onClick={() => handleContact(socio)}>Contactar</Button>
-                            {' '}
-                            {userData.nombre === socio && <Button variant="warning" onClick={() => handleEditNovedad(id, novedad)}>Editar</Button>}
-                            {' '}
-                            <Button variant="danger" onClick={() => handleDeleteNovedad(id)}>Borrar</Button>
-                        </Card.Body>
-                    </Card>
-                    </div>
-                ))}
-            </div>
-            <Row className="align-items-center">
-            
-            <Form onSubmit={handleAddNovedad}>
-            <Col xs="auto">
-                <Form.Group controlId="novedadInput">
-                    <Form.Label>Agregar Novedad</Form.Label>
-                    <Form.Control
-                        as="textarea"
-                        rows={3}
-                        value={newNovedad}
-                        onChange={(e) => setNewNovedad(e.target.value)}
-                    />
-                </Form.Group>
-                </Col>
-                <Col xs="auto">
-                <Form.Group controlId="imageInput" className="mt-3">
-                    <Form.Label>Agregar Imagen</Form.Label>
-                    <Form.Control
-                        type="file"
-                        accept="image/*"
-                        onChange={(e) => setNewImage(URL.createObjectURL(e.target.files[0]))}
-                    />
-                </Form.Group>
-                </Col>
-                <Button variant="primary" type="submit" className="mt-3">
-                    Agregar Novedad
-                </Button>
-            </Form>
-            
-            </Row>
+            <h2>Tablero de Información</h2>
+            <Tabs activeKey={key} onSelect={(k) => setKey(k)} className="mb-3">
+                <Tab eventKey="novedades" title="Novedades">
+                    <Row>
+                        {novedades.map(({ id, nombre, lote, novedad, color }) => (
+                            <Col key={id} md={3} className="mb-3">
+                                <Card className="shadow-sm" style={{ backgroundColor: color }}>
+                                    <Card.Body>
+                                        <Card.Title>{nombre} - Lote: {lote}</Card.Title>
+                                        <Card.Text>{novedad}</Card.Text>
+                                        <Button variant="danger" size="sm" onClick={() => handleDelete(id, 'novedades')}>Eliminar</Button>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                        ))}
+                    </Row>
+                </Tab>
+                <Tab eventKey="telefonos" title="Teléfonos Útiles">
+                    <h5>Categorías</h5>
+                    <Form.Select className="mb-3" onChange={(e) => setCategoriaSeleccionada(e.target.value)}>
+                        {categorias.map(categoria => (
+                            <option key={categoria} value={categoria}>{categoria}</option>
+                        ))}
+                    </Form.Select>
+                    <h5 className="mt-3">Listado</h5>
+                    <Row>
+                        {telefonosUtiles.filter(({ categoria }) => categoria === categoriaSeleccionada).map(({ id, categoria, nombre, telefono, email }) => (
+                            <Col key={id} md={3} className="mb-3">
+                                <Card className="shadow-sm">
+                                        <Card.Body>
+                                        <Card.Title>{categoria} - {nombre}</Card.Title>
+                                        <Card.Text>Teléfono: {telefono} - Email: {email}</Card.Text>
+                                        <div>
+                                            {[1, 2, 3, 4, 5].map(star => (
+                                                <FaStar key={star} color={rating[id] >= star ? "gold" : "gray"} onClick={() => setRating({ ...rating, [id]: star })} />
+                                            ))}
+                                        </div>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                        ))}
+                    </Row>
+                    <Button variant="success" className="mt-3" onClick={handleAddTelefono}>Agregar Teléfono</Button>
+                </Tab>
+            </Tabs>
         </div>
     );
 };
