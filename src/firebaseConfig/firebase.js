b7433941863e55c249457c7f918c0aaec9eb87de
defilippi82 <f.defilippi@gmail.com>
2026-03-30 09:13:12 -0300

    login

--- src/firebaseConfig/firebase.js
+++ src/firebaseConfig/firebase.js
@@ -1,61 +1,3 @@
-/*// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore, collection, addDoc, onSnapshot, enableIndexedDbPersistence } from "firebase/firestore";
-import { getMessaging, getToken } from "firebase/messaging";
-import { getAnalytics } from "firebase/analytics";
-
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  
-
-
-  apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP_ID,
-  measurementId: import.meta.env.VITE_MEASUREMENT_ID
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
-export const auth = getAuth(app);
-export const analytics = getAnalytics(app);
-// Initialize Firebase Cloud Messaging and get a reference to the service
-export const messaging = getMessaging(app);
-
-enableIndexedDbPersistence(db).catch((err) => {
-  if (err.code === 'failed-precondition') {
-    console.log("Persistencia no disponible (múltiples pestañas abiertas)");
-  } else if (err.code === 'unimplemented') {
-    console.log("Persistencia no soportada en este navegador");
-  } else {
-    console.log("Error al activar persistencia offline: ", err);
-  }
-});
-
-export const obtenerTokenFCM = async () => {
-  try {
-    const currentToken = await getToken(messaging, { vapidKey: 'BC1dFTH3QJeInZ8LL-2ZrBj6EXE8iWmDu7PDfDGhx7LiADYJ_KjzZdK-izhIaPOpmI2qQ0cveH_fl5orZ1znFTw' });
-    if (currentToken) {
-      console.log('Token de FCM:', currentToken);
-      return currentToken;
-    } else {
-      console.log('No se pudo obtener el token. Solicita permiso para recibir notificaciones primero.');
-      return null;
-    }
-  } catch (error) {
-    console.error('Error al obtener el token de FCM:', error);
-    return null;
-  }
-};*/
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore, enableIndexedDbPersistence } from "firebase/firestore"; // Quitamos lo que no usas aquí
@@ -70,7 +12,7 @@ const firebaseConfig = {
   messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_APP_ID,
   measurementId: import.meta.env.VITE_MEASUREMENT_ID
-  
+
 };
 
 const app = initializeApp(firebaseConfig);
