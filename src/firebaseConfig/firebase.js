914bc92d416fd1505bb0578d5e9d69e929104fda
defilippi82 <f.defilippi@gmail.com>
2026-03-29 22:02:26 -0300

    firebase.js

--- src/firebaseConfig/firebase.js
+++ src/firebaseConfig/firebase.js
@@ -1,4 +1,4 @@
-// Import the functions you need from the SDKs you need
+/*// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore, collection, addDoc, onSnapshot, enableIndexedDbPersistence } from "firebase/firestore";
@@ -53,6 +53,45 @@ export const obtenerTokenFCM = async () => {
     }
   } catch (error) {
     console.error('Error al obtener el token de FCM:', error);
+    return null;
+  }
+};*/
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore, enableIndexedDbPersistence } from "firebase/firestore"; // Quitamos lo que no usas aquí
+import { getMessaging, getToken } from "firebase/messaging";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const analytics = getAnalytics(app);
+export const messaging = getMessaging(app);
+
+// COMENTA ESTO TEMPORALMENTE
+/*
+enableIndexedDbPersistence(db).catch((err) => {
+  console.log("Error persistencia: ", err.code);
+});
+*/
+
+export const obtenerTokenFCM = async () => {
+  try {
+    const currentToken = await getToken(messaging, { 
+      vapidKey: 'BC1dFTH3QJeInZ8LL-2ZrBj6EXE8iWmDu7PDfDGhx7LiADYJ_KjzZdK-izhIaPOpmI2qQ0cveH_fl5orZ1znFTw' 
+    });
+    return currentToken;
+  } catch (error) {
     return null;
   }
 };
