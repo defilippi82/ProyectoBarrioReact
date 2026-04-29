5b422907d679c7c9cfe3e3f45bd3f2bddc1430ba
Federico H. De Filippi <53954795+defilippi82@users.noreply.github.com>
2026-04-29 10:47:55 -0300

    footer

--- src/components/Views/Footer.jsx
+++ src/components/Views/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const Footer = () => {
   return (
     
-      <div className="footer">
+      <div className="footer bg-image text-light text-center p-4">
     <hr className="sm-4" />
 
       
